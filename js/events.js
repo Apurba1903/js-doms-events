951a87244a089cdab14dd907a40ff8aaf6cbddfe
Apurba1903 <apurba1903@gmail.com>
2023-08-17 03:14:47 +0600

    event handeler

--- js/events.js
+++ js/events.js
@@ -1,1 +1,26 @@
 console.log('This is seperate JS file.');
+
+// Important
+function makeYellow() {
+    document.body.style.backgroundColor = 'yellow';
+}
+
+function makeRed() {
+    document.body.style.backgroundColor = 'red';
+}
+
+const pinkButton = document.getElementById('make-pink');
+pinkButton.addEventListener('click', makePink);
+function makePink() {
+    document.body.style.backgroundColor = 'pink';
+}
+
+const greenButton = document.getElementById('make-green');
+greenButton.addEventListener('click', function makeGreen() {
+    document.body.style.backgroundColor = 'green';
+});
+
+// Important 
+document.getElementById('make-goldenrod').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'goldenrod';
+});
